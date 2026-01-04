f485574c4fc91e2a8d2ead788589b9bf74f53e7b
Daniel Fokou <danieluokof@gmail.com>
2026-01-03 19:09:35 -0500

    feat: Onboarding v2 avec fonds animés et design compact

--- lib/core/constants/assets.dart
+++ lib/core/constants/assets.dart
@@ -2,8 +2,34 @@
 class AppAssets {
   AppAssets._();
 
-  // Images
+  // Logos
   static const String google_Drive_logo = 'assets/images/png/Google_Drive_logo.png';
   static const String iCloud_logo = 'assets/images/png/ICloud_logo.png';
   static const String oneDrive_logo = 'assets/images/png/OneDrive_logo.png';
+
+  // Background images pour onboarding
+  static const String background1 = 'assets/images/Backgrounds/anders-jilden-O85h02qZ24w-unsplash.jpg';
+  static const String background2 = 'assets/images/Backgrounds/annie-spratt-hX_hf2lPpUU-unsplash.jpg';
+  static const String background3 = 'assets/images/Backgrounds/celina-sakaguchi-xj0XkgV43ao-unsplash.jpg';
+  static const String background4 = 'assets/images/Backgrounds/chris-lawton-5IHz5WhosQE-unsplash.jpg';
+  static const String background5 = 'assets/images/Backgrounds/chuttersnap-NOVBDRiTdO4-unsplash.jpg';
+  static const String background6 = 'assets/images/Backgrounds/corinne-kutz-7ZtPwmSKj6U-unsplash.jpg';
+  static const String background7 = 'assets/images/Backgrounds/cristina-gottardi-CSpjU6hYo_0-unsplash.jpg';
+  static const String background8 = 'assets/images/Backgrounds/dmytro-tolokonov-Jq3WI9IQgEs-unsplash.jpg';
+  static const String background9 = 'assets/images/Backgrounds/evie-s-kqJfP-lrl-8-unsplash.jpg';
+  static const String background10 = 'assets/images/Backgrounds/igor-son-FV_PxCqgtwc-unsplash.jpg';
+
+  // Liste de tous les backgrounds pour accès facile
+  static const List<String> onboardingBackgrounds = [
+    background1,
+    background2,
+    background3,
+    background4,
+    background5,
+    background6,
+    background7,
+    background8,
+    background9,
+    background10,
+  ];
 }
